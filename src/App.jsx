e35b8a7a2badff3ab9477a03d76db89badfcca0c
dhriti-desai <dhritidesai100@gmail.com>
2026-02-10 20:22:21 -0500

    Change fonts to Dancing Script for headings and Merriweather serif for body

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './pages/Home'
 import About from './pages/About'
+import Mission from './pages/Mission'
 import Team from './pages/Team'
 import Events from './pages/Events'
 import Newsletter from './pages/Newsletter'
@@ -19,6 +20,7 @@ export default function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
+            <Route path="/mission" element={<Mission />} />
             <Route path="/team" element={<Team />} />
             <Route path="/events" element={<Events />} />
             <Route path="/newsletter" element={<Newsletter />} />
